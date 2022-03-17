8d5006af29d31a582d2db42da0f779ac3046c74a
rishabhs478 <93025052+rishabhs478@users.noreply.github.com>
2022-03-17 13:40:48 +0530

    Update context.js

--- src/context.js
+++ src/context.js
@@ -20,7 +20,11 @@ const reducer = (state, action) => {
       case "UPDATE_CONTACT":
       return {
         ...state,
-       contacts:[action.payload , ...state.contacts]  
+        contacts: state.contacts.map((contact) =>
+          contact.id === action.payload.id
+            ? (contact = action.payload)
+            : contact
+        ),
       };
     default:
       return state;
